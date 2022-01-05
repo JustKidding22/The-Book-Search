32a1fa66a9f590b277c0d9c2edd951bf0529254c
James K <projdk90@gmail.com>
2022-01-05 14:07:17 -0800

    updated queries and connection

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
 import gql from "graphql-tag";
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      bookCount
+      savedBooks {
+        title
+        bookId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  function (err) {
+    if (err) {
+      console.error(err);
+    }
+  }
+);
 
 module.exports = mongoose.connection;
